ebaa7f51814f309af48a5cd796ec876152a3cdd9
darshangujraticodes <darshangujrati5@gmail.com>
2024-05-29 01:40:09 +0530

    rser

--- 01_Basics/python_strings.py
+++ 01_Basics/python_strings.py
@@ -1,7 +1,6 @@
 
 
 """
-
 curiosity question
 
 find the difference between this strings
@@ -12,5 +11,50 @@ str('chai')
 
 print('chai')   
 
-
 """
+
+
+string_val = 'lemon tea'
+
+print(string_val)
+
+print(string_val[2])
+
+print(string_val[0:5])
+
+string_val2 = '01234567898'
+
+print(string_val2[2:])
+
+print(string_val2[:6])
+
+print(string_val2[1:8:2])
+
+print(string_val2[-1])
+
+#string methods
+
+string_val3 = "   Darshan                   is combination of         Frontend Developer and Backend Developer          "
+
+print(string_val3.lower())
+
+print(string_val3.upper())
+
+# note python string is case sensitive 
+print(string_val3.count('Developer')) # output will be 4
+
+print(string_val3.count('developer')) # ouput will be zero 
+
+print(string_val3.replace('Darshan','Rohit'))
+
+# remve unwanted space in string specifiacly we we fetch data from customer input form
+
+print(string_val3.strip())
+
+
+
+
+
+
+
+
