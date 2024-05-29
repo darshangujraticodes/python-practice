4ef019d04e9bb9c4b8d817373cdbb08fbd2d9961
darshangujraticodes <darshangujrati5@gmail.com>
2024-05-29 19:59:18 +0530

    python strings

--- 01_Basics/python_strings.py
+++ 01_Basics/python_strings.py
@@ -22,7 +22,7 @@ print(string_val[2])
 
 print(string_val[0:5])
 
-string_val2 = '01234567898'
+string_val2 = '0123456789'
 
 print(string_val2[2:])
 
@@ -41,25 +41,29 @@ print(string_val3.lower())
 print(string_val3.upper())
 
 # note python string is case sensitive 
-print(string_val3.count('Developer')) # output will be 4
+print('count() = ',string_val3.count('Developer')) # output will be 4
 
 print(string_val3.count('developer')) # ouput will be zero 
 
-print(string_val3.replace('Darshan','Rohit'))
+print('replace() = ',string_val3.replace('Darshan','Rohit'))
 
 # will help to remove unknowing space of start and end of string specificaly we fetch data from customer input form
 
-print(string_val3.strip())
+print('strip() = ',string_val3.strip())
 
-chai = "Ginger, Lemon, Masala, Mint, Green"
+chai = "Ginger Lemon Masala Mint Green"
 
-print(chai.split()) #default split built in funcation
+print('split() = ',chai.split()) #default split built in funcation
 
 print(chai.split(', ')) #defa
 
-print(chai.find('Mint')) #defa
+print('len() = ',len(chai)) #length 
 
-print("".join(chai))
+print('find()',chai.find('Mint')) #fetch the letter postion where data is found in string
+
+prog_lang = ['java','python','javascript','c++']
+
+print("-".join(prog_lang))
 
 temp1 = 45
 
@@ -84,7 +88,26 @@ print(order.format(order_figure, type_chai))
 
 
 
+# string loops
+
+print('string loops')
 
 
+str_val2 = 'darshan full stack developer'
 
+for k in str_val2:
+    print(k)
+
+
+#advance string concepts
+
+str_val4 = 'He said, \"Darshan is Full Stack Developer " '
+
+print(str_val4)
+
+
+web_url = "c:\darshan\python-practice"
+print(web_url)
+
+print('darshan' in str_val2)
 
