723659681ecd87484cc015100bcb94adf3eced01
darshangujraticodes <darshangujrati5@gmail.com>
2024-05-31 19:12:11 +0530

    python conditions

--- 02_Controlflow/python_condition.py
+++ 02_Controlflow/python_condition.py
@@ -9,43 +9,43 @@
 
 # print(person_age_str, type(person_age_str))
 
-# person_age = int(input('Enter Person Age = '))
+person_age = int(input('Enter Person Age = '))
 
-# if (person_age <= 12):
-#     print("He/ She is in Child Category")
+if (person_age <= 12):
+    print("He/ She is in Child Category")
 
-# elif (person_age >= 13 and person_age <= 19 ):
-#     print("He/ She is in Teenage Category")
+elif (person_age >= 13 and person_age <= 19 ):
+    print("He/ She is in Teenage Category")
 
-# elif (person_age >= 20 and person_age <= 59):
-#     print("He/ She is in Adult Category")
+elif (person_age >= 20 and person_age <= 59):
+    print("He/ She is in Adult Category")
     
-# else:
-#     print("He/ She is in Senior Category")
+else:
+    print("He/ She is in Senior Category")
     
 
 
 # problem 2 : movie ticket adult = $12 child = $8 and on wednesday $2 discount
 
-# age = int(input('Enter your Age = '))
+age = int(input('Enter your Age = '))
 
-# day = input('Enter Booking Day =')
+day = input('Enter Booking Day =')
 
-# compare_day = day.lower()
+compare_day = day.lower()
 
-# print(compare_day)
+print(compare_day)
 
-# if (compare_day == 'wednesday'):
-#     if (age < 18):
-#         print('Your Ticket Price is $6')
-#     else:
-#         print('Your Ticket Price is $10')
+if (compare_day == 'wednesday'):
+    if (age < 18):
+        print('Your Ticket Price is $6')
+    else:
+        print('Your Ticket Price is $10')
 
-# else:
-#     if (age < 18):
-#         print('Your Ticket Price is $8')
-#     else:
-#         print('Your Ticket Price is $12')
+else:
+    if (age < 18):
+        print('Your Ticket Price is $8')
+    else:
+        print('Your Ticket Price is $12')
 
 
 #problem 3 : 
