f8e981f2ea6c237d275c3448ae22af6e5d79cacb
darshangujraticodes <darshangujrati5@gmail.com>
2024-05-31 19:08:38 +0530

    python conditions

---
+++ 02_Controlflow/python_condition.py
@@ -0,0 +1,51 @@
+#python condition statment
+
+#problem 1 :   child (1-12) , Teenage (13-19) , Adult (20-59) and Senior (60+)
+
+
+# note when get value in user input the data is in datatype of string so we need to convert into integrer to perform maths operation
+
+# person_age_str = input('Enter Person Age = ')
+
+# print(person_age_str, type(person_age_str))
+
+# person_age = int(input('Enter Person Age = '))
+
+# if (person_age <= 12):
+#     print("He/ She is in Child Category")
+
+# elif (person_age >= 13 and person_age <= 19 ):
+#     print("He/ She is in Teenage Category")
+
+# elif (person_age >= 20 and person_age <= 59):
+#     print("He/ She is in Adult Category")
+    
+# else:
+#     print("He/ She is in Senior Category")
+    
+
+
+# problem 2 : movie ticket adult = $12 child = $8 and on wednesday $2 discount
+
+# age = int(input('Enter your Age = '))
+
+# day = input('Enter Booking Day =')
+
+# compare_day = day.lower()
+
+# print(compare_day)
+
+# if (compare_day == 'wednesday'):
+#     if (age < 18):
+#         print('Your Ticket Price is $6')
+#     else:
+#         print('Your Ticket Price is $10')
+
+# else:
+#     if (age < 18):
+#         print('Your Ticket Price is $8')
+#     else:
+#         print('Your Ticket Price is $12')
+
+
+#problem 3 : 
