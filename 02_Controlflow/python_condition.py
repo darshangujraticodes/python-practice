7839b58b0da50dabde01ab114d165c52dc72029a
darshangujraticodes <darshangujrati5@gmail.com>
2024-06-01 10:55:00 +0530

    python condition

--- 02_Controlflow/python_condition.py
+++ 02_Controlflow/python_condition.py
@@ -5,9 +5,11 @@
 
 # note when get value in user input the data is in datatype of string so we need to convert into integrer to perform maths operation
 
-person_age_str = input('Enter Person Age = ')
+# person_age_str = input('Enter Person Age = ')
 
-print(person_age_str, type(person_age_str))
+# print(person_age_str, type(person_age_str))
+
+print('\n Person age Condition Problem 1')
 
 person_age = int(input('Enter Person Age = '))
 
@@ -22,10 +24,13 @@ elif (person_age >= 20 and person_age <= 59):
     
 else:
     print("He/ She is in Senior Category")
-    
 
+    
+# ------------------------------
 
 # problem 2 : movie ticket adult = $12 child = $8 and on wednesday $2 discount
+
+print('\n Movie ticket Booking Problem 2')
 
 age = int(input('Enter your Age = '))
 
@@ -47,10 +52,16 @@ else:
     else:
         print('Your Ticket Price is $',adult_ticket_price)
 
+
+# ------------------------------
+
+
 '''
 problem 3 : student grade system  A (90-100), B (80-89),
  C (70,79), D (60-69), F (below 60)
 '''
+
+print('\n Student Marks Problem 3')
 
 student_marks = int(input('Enter Your Marks = '))
 
@@ -74,7 +85,12 @@ else:
     print('Student Grade is F')
 
 
+
+# ------------------------------
+
 # coffee order problem
+
+print('\n Coffee Order Problem 4')
 
 coffee_order_size = input('Enter Coffee order Size = ')
 
@@ -89,7 +105,58 @@ else:
     print(coffee_order)
 
 
+# ------------------------------
 
+
+# password validtaion
+
+print('\n Valid Password Checker Problem 5')
+
+password = input('Enter Your password = ')
+
+# char = 'Darshan$85'
+# not convert boolean value in opposite form true -> false an vice versa
+#any() method  return true  or false on condition satisfy
+
+
+def password_check(password_input):
+    special_symbols = ['@','#','$','*','!']
+    val = True
+
+    if (len(password_input) < 6 ):
+        print('Minimum length limit of Password is 6 ! ')
+        val = False
+        exit()
+    
+    if (len(password_input) > 13 ):
+        print('Maximum length limit of Password is 12 ! ')
+        val = False
+        exit()
+
+    if not any(char.isdigit() for char in password_input  ):
+        print('Password must have one Numeric Digit')
+        val = False
+        exit()
+
+    if not any(char.isupper() for char in password_input):
+        print('Password must have one Uppercase Value')
+        val = False
+        exit()
+        
+    if not any(char.islower() for char in password_input):
+        print('Password must have one Lowercase Value')
+        val = False
+        exit()
+                
+    if not any(char in special_symbols for char in password_input):
+        print('Password must have one Special Symbols @,#,$,* and !  ')
+        val = False
+        exit()
+
+    if val:
+        return print('Passoword is Valid.')
+    
+password_check(password)
 
 
 
