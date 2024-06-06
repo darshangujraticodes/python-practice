b86e8c2fa5b264420423d5cffc27c1fe025f9f81
darshangujraticodes <darshangujrati5@gmail.com>
2024-06-06 20:13:53 +0530

    python oops

--- 04_OOPS/python_inheritance.py
+++ 04_OOPS/python_inheritance.py
@@ -14,7 +14,7 @@ class Circle:
     def circle_info(self):
         print('Radius =',self.radius)
         area = round(math.pi * self.radius * self.radius,2)
-        circf = round(2 + math.pi * self.radius,2)
+        circf = round(2 * math.pi * self.radius,2)
         print(f'Area = {area}, Circumference = {circf}')
 
 
@@ -31,16 +31,18 @@ class CircleData(Circle):
         cirf =  round(2 * math.pi * self.radius,2)
         return cirf
     
-    
+
 c1 = Circle(10)
 c2 = CircleData(20)
+c3 = CircleData(30)
+
 
 # parent class cannot access child class method 
 # print('Area =',c1.find_area(),' Circumference = ',c1.find_circumference() )
 
 
 #but child class can access parent class method
-c2.circle_info()
+c3.circle_info()
 print('Area =',c2.find_area(),' Circumference = ',c2.find_circumference() )
 
 
