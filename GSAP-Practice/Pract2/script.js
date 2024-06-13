78c77a1546c41ebf02d610d933d21242f8c904b0
darshangujraticodes <darshangujrati5@gmail.com>
2024-06-13 20:09:32 +0530

    gsap

---
+++ GSAP-Practice/Pract2/script.js
@@ -0,0 +1,23 @@
+gsap.from(".box1", {
+  opacity: 0,
+  rotate: 360,
+  scale: 0,
+  duration: 2,
+  delay: 1,
+});
+
+gsap.from(".box2", {
+  opacity: 0,
+  rotate: 360,
+  scale: 0,
+  duration: 2,
+  delay: 1,
+});
+
+gsap.from(".box3", {
+  opacity: 0,
+  rotate: 360,
+  scale: 0,
+  duration: 2,
+  delay: 1,
+});
