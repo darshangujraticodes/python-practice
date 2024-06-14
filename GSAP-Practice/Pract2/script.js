cdc5f8bf369976fa15f67d37cb48b75435427a2e
darshangujraticodes <darshangujrati5@gmail.com>
2024-06-14 16:02:28 +0530

    gsap anime

--- GSAP-Practice/Pract2/script.js
+++ GSAP-Practice/Pract2/script.js
@@ -1,12 +1,11 @@
-gsap.from(".box1", {
+gsap.from(".headText", {
   opacity: 0,
-  rotate: 360,
-  scale: 0,
+  y: -20,
   duration: 2,
   delay: 1,
 });
 
-gsap.from(".box2", {
+gsap.from(".box1", {
   opacity: 0,
   rotate: 360,
   scale: 0,
@@ -19,5 +18,63 @@ gsap.from(".box3", {
   rotate: 360,
   scale: 0,
   duration: 2,
-  delay: 1,
+  scrollTrigger: {
+    trigger: ".section3 .box3",
+    scroller: "body",
+    start: "top 60%",
+  },
+});
+
+gsap.from(".leftAnime", {
+  opacity: 0,
+  x: 50,
+  duration: 0.7,
+  stagger: 0.4,
+  scrollTrigger: {
+    trigger: ".leftAnime",
+    scroller: "body",
+    // markers: true,
+    start: "top 50%",
+  },
+});
+
+gsap.from(".rightAnime", {
+  // scale: 0,
+  opacity: 0,
+  x: -50,
+  duration: 1,
+  stagger: 0.4,
+  scrollTrigger: {
+    trigger: ".leftAnime",
+    scroller: "body",
+    // markers: true,
+    start: "top 50%",
+  },
+});
+
+gsap.from(".upAnime", {
+  // scale: 0,
+  opacity: 0,
+  y: 50,
+  duration: 1,
+  stagger: 0.4,
+  scrollTrigger: {
+    trigger: ".upAnime",
+    scroller: "body",
+    start: "top 40%",
+  },
+});
+
+gsap.from(".downAnime", {
+  // scale: 0,
+  opacity: 0,
+  y: -50,
+  duration: 1,
+  stagger: 0.4,
+  scrollTrigger: {
+    trigger: ".downAnime",
+    scroller: "body",
+    // markers: true,
+    start: "top 50%",
+  },
 });
